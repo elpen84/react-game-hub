c87ccbc1f449e2f60aefe68e716f88e0d787cea2
Eddie Price <elplist@gmail.com>
2023-08-08 22:53:24 -0700

    Skeletons not working

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -31,18 +31,25 @@ const useGames = () => {
 
     const [games, setGames] =useState<Game[]>([])
     const [error, setError] =useState('')
+    const [isLoading, setLoading] = useState(false)
 
+
+setLoading(true);
     useEffect(() => {
         apeClient.get<FetchGamesResponse>('/games', {signal: controller.signal})
-        .then(res => setGames(res.data.results))
+        .then(res => {
+            setGames(res.data.results);
+            setLoading(false);
+        })
         .catch(err =>  {
             if (err instanceof CanceledError) return;
             setError(err.message)})
+            setLoading(false);
 
         return () => controller.abort();
     },[])
 
-    return { games, error}
+    return { games, error, isLoading}
 }
 
 export default useGames
