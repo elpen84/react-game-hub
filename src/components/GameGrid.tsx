d7cc75a89bc1c853f05aba3a1c3f59fd0d44315f
Eddie Price <elplist@gmail.com>
2023-08-09 21:16:16 -0700

    added container component

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -3,6 +3,7 @@ import useGames from '../hooks/useGames';
 import { SimpleGrid, Text } from '@chakra-ui/react';
 import GameCard from './GameCard';
 import GameCardSkeleton from './GameCardSkeleton';
+import GameCardContainer from './GameCardContainer';
 
 
 
@@ -17,10 +18,10 @@ const GameGrid = () => {
   return (
 <>
 <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl: 5}} padding={10} spacing={10}>
-        {isLoading && skeletons.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
+        {isLoading && skeletons.map((skeleton) => <GameCardContainer><GameCardSkeleton key={skeleton} /></GameCardContainer>)}
         {games.map((game)=> 
-        <GameCard key={game.id} game={game} 
-        />)}
+        <GameCardContainer><GameCard key={game.id} game={game} 
+        /></GameCardContainer>)}
     </SimpleGrid> 
 </>
   )
