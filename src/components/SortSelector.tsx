eb8051fcdf6a3f97c7881f25d3c30a44c9d75f93
Eddie Price <elplist@gmail.com>
2023-12-31 21:37:52 -0800

    sortselector Component

---
+++ src/components/SortSelector.tsx
@@ -0,0 +1,22 @@
+import { Button, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react'
+import React from 'react'
+import { BsChevronBarDown } from 'react-icons/bs'
+
+export const SortSelector = () => {
+    return (
+        <Menu>
+            <MenuButton  as={Button} rightIcon={<BsChevronBarDown />}>
+               Order:
+                </MenuButton>
+            <MenuList>
+                <MenuItem>Relevance</MenuItem>
+                <MenuItem>Date Added</MenuItem>
+                <MenuItem>Name</MenuItem>
+                <MenuItem>Release Date</MenuItem>
+                <MenuItem>Popularity</MenuItem>
+                <MenuItem>Average Rating</MenuItem>
+            </MenuList>
+        </Menu>
+        
+      )
+}
